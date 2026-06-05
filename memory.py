0f7358a0564ff615343dc900f24002a91ef51dff
unknown <283878@student.pwr.edu.pl>
2026-06-05 21:32:19 +0200

    Load and remove pages from RAM

--- memory.py
+++ memory.py
@@ -6,6 +6,10 @@ class Page():
         self.arrivalTime = None
         self.accessCount = 0
         self.mostRecentAccess = None
+    def reset(self):
+        self.arrivalTime = None
+        self.accessCount = 0
+        self.mostRecentAccess = None
 
 class VRAM():
     def __init__(self,size: int = 16):
@@ -17,6 +21,7 @@ class HardDrive():
         self.requiredMemory = requiredMem
         self.processPages = pages
         self.pageRefferences = refs
+        self.nProc = len(self.pageRefferences)
 
 class MMU():
     def __init__(self):
@@ -26,12 +31,25 @@ class MMU():
         for page in self.vram.pages:
             if pageID == page.page_number: return True # if page is loaded into memory, page check OK
         return False # else - page fault
-    def pageToRemove(self): # select page to free from memory
+    def pageToRemove(self): # select frame to free from memory
         for i in range(len(self.vram.pages)):
             if self.vram.pages[i] == None:
-                return i # by default return address of the first empty memory segment
-        else: return numpy.random.randint(self.vram.size-1) # if no segments are empty, return random address
+                return i # by default return address of the first empty memory frame
+        else: return numpy.random.randint(self.vram.size-1) # if no frames are empty, return random address
     def removePage(self):
-        pass
-    def loadPage(self):
-        pass
+        index = self.pageToRemove()
+        self.vram.pages[index].reset()
+        self.vram.pages[index]=None
+        return index
+    def loadPage(self, pageID):
+        index = self.pageToRemove()
+        self.vram.pages[index] = self.disk.processPages[pageID]
+        return index
+    def run(self):
+        counter = 0
+        while counter < self.disk.nProc:
+            pageID = self.disk.pageRefferences[counter]
+            if not self.pageCheckOK(pageID):
+                print(f"""Frame {self.removePage()} released""")
+            print(f"""Loaded Page {pageID} into frame {self.loadPage(pageID)}""")
+            counter +=1
