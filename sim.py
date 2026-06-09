070e320bbe5eabae39c2e895f675e7895fc35f82
ambroz283878 <283878@student.pwr.edu.pl>
2026-06-09 22:10:27 +0200

    Algorithm selection

--- sim.py
+++ sim.py
@@ -15,4 +15,16 @@ drive = memory.HardDrive(ramSize,memory.generatePages(pageNum), refs)
 
 memoryManagementUnit=memory.MMU(vram,drive)
 
+mode = input("Wybierz algorytm (FIFO/LRU/random)")
+if mode not in ["FIFO","LRU","random"]:
+    print(f"Nieznany algorytm ({mode})")
+    exit
+match mode:
+    case "FIFO":
+        memoryManagementUnit=memory.FIFO(vram,drive)
+    case "LRU":
+        memoryManagementUnit=memory.LRU(vram,drive)
+    case "random":
+        memoryManagementUnit=memory.MMU(vram,drive)
+
 memoryManagementUnit.run()
