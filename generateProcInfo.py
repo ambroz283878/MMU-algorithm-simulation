0b0d5872855faccb78083536c5e0a5556537497c
ambroz283878 <283878@student.pwr.edu.pl>
2026-06-10 00:30:58 +0200

    New distribution

--- generateProcInfo.py
+++ generateProcInfo.py
@@ -4,6 +4,8 @@ procInfo = memory.generateReferences(
     totalPages=50,
     workingSetSize=16,
     nRefs=5000,
+    localityWeight=0.75,
+    spatialWeight=0.1
 )
 path="procInfo.json"
 with open(path, "w") as file:
